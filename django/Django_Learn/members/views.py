a25b39e4954c6d9fe7f4c750f987bdd37cc80886
Ivy-Chelsea <ivychelsea20@gmail.com>
2025-01-03 11:33:12 +0300

    create templates folder, add a new template, create a new view and migrate the members app in setting.py

--- django/Django_Learn/members/views.py
+++ django/Django_Learn/members/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def members(request):
     return HttpResponse("Hello world!")
+
+
+def first(request):
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
